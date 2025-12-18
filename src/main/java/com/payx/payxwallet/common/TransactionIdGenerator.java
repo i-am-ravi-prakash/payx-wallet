5834c3be029b6bbb7bd90582e8dc119174498841
Ravi Prakash <raviprakash951998@gmail.com>
2025-12-18 09:30:48 +0530

    [CodePilotAI] Add Logger to TransactionIdGenerator (58f4ccfc-a237-48be-b6dc-c9312f585bf2)

--- src/main/java/com/payx/payxwallet/common/TransactionIdGenerator.java
+++ src/main/java/com/payx/payxwallet/common/TransactionIdGenerator.java
@@ -1,12 +1,15 @@
+```java
 package com.payx.payxwallet.common;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.logging.Logger;
 
 public class TransactionIdGenerator {
 
     private static final ZoneId ZONE_ID = ZoneId.of("Asia/Kolkata");
+    private static final Logger logger = Logger.getLogger(TransactionIdGenerator.class.getName());
 
     public static String generate(String serviceType, String node){
         ZonedDateTime now = ZonedDateTime.now(ZONE_ID);
@@ -17,6 +20,10 @@ public class TransactionIdGenerator {
         int millisInt = now.getNano() / 1000000;
         String millis = String.format("%03d", millisInt);
 
-        return serviceType + date + "." + time + "." + node + second + millis;
+        String transactionId = serviceType + date + "." + time + "." + node + second + millis;
+        logger.info("Generated Transaction ID: " + transactionId);
+
+        return transactionId;
     }
 }
+```
